5f1188d832bdb92963b55ce3c72d24487899743f
Marvin Joseph <147320510+ImPresence2001@users.noreply.github.com>
2024-10-30 02:02:49 +0800

    Initial Commit

---
+++ Coftea/admin.py
@@ -0,0 +1,45 @@
+from django.contrib import admin
+from .models import Category, Product, Inventory, OrderTransaction, OrderItem
+
+# Category Admin
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_id', 'category_name')
+    search_fields = ('category_name',)
+
+
+# Product Admin
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_id', 'product_name', 'price', 'category')
+    search_fields = ('product_name',)
+    list_filter = ('category',)
+
+
+# Inventory Admin
+@admin.register(Inventory)
+class InventoryAdmin(admin.ModelAdmin):
+    list_display = ('inventory_id', 'product', 'quantity')
+    search_fields = ('product__product_name',)
+
+
+# OrderItem Inline (for displaying OrderItems within an OrderTransaction)
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1  # Number of extra blank items to display
+
+
+# OrderTransaction Admin
+@admin.register(OrderTransaction)
+class OrderTransactionAdmin(admin.ModelAdmin):
+    list_display = ('order_id', 'transaction_date', 'total_price', 'user')
+    search_fields = ('user__username', 'order_id')
+    list_filter = ('transaction_date',)
+    inlines = [OrderItemInline]  # Adding OrderItem inline to display items within each order
+
+
+# OrderItem Admin
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order_item_id', 'order', 'product', 'quantity', 'price')
+    search_fields = ('order__order_id', 'product__product_name')
